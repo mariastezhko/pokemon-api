7e78c3b92c4b0a92bc445febd4dc65f12f185037
Maria Stezhko <stezhko@gmail.com>
2018-02-16 12:47:16 -0800

    feat: Add service that delivers data about a pokemon

---
+++ src/app/http.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class HttpService {
+
+  constructor(private _http: HttpClient) {
+      this.getPokemon();
+  }
+  getPokemon() {
+      let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+      bulbasaur.subscribe(data => console.log("Got pokemon!", data));
+      bulbasaur.subscribe(data => console.log("Bulbasaur's abilities are", data.abilities[0].ability.name, "and", data.abilities[1].ability.name ));
+  }
+
+}
