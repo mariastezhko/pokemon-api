49263faa4a2be9578a607ac494906ecf90bff016
Maria Stezhko <stezhko@gmail.com>
2018-02-16 15:02:58 -0800

    feat: Add fetching data about how many pokemons share the ability

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -6,18 +6,16 @@ export class HttpService {
 
   constructor(private _http: HttpClient) {
       this.getPokemon();
-      this.getPokemons();
   }
   getPokemon() {
       let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
-      bulbasaur.subscribe(data => console.log("Got pokemon!", data));
-      bulbasaur.subscribe(data => console.log("Bulbasaur's abilities are",
-                data.abilities[0].ability.name, "and", data.abilities[1].ability.name ));
+      bulbasaur.subscribe(data => {
+        console.log("Got pokemon!", data);
+        console.log("Bulbasaur's abilities are",
+                  data.abilities[0].ability.name, "and", data.abilities[1].ability.name );
+        let pokemons = this._http.get(data.abilities[0].ability.url);
+        pokemons.subscribe(data => console.log(data.pokemon.length, `Pokemons have the chlorophyll ability`));
 
+      });
   }
-  getPokemons() {
-    let pokemons = this._http.get('https://pokeapi.co/api/v2/ability/34/'); // data.abilities[0].ability.url
-    pokemons.subscribe(data => console.log(data.pokemon.length, `Pokemon have the chlorophyll ability`));
-  }
-
 }
